a61617563a292453c6b8cfb208a57385dc2da8ee
Michał Panfil <it.panfil@gmail.com>
2023-05-06 17:57:46 +0200

    try pillow and opencv

--- flask-web-api/api.py
+++ flask-web-api/api.py
@@ -1,30 +1,126 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import os,re, datetime
+from models import Book
+import db
+
 app = Flask(__name__)
 
-users = [
- {"id": 1, "name": "John Doe"},
- {"id": 2, "name": "Jane Doe"},
- {"id": 3, "name": "John Smith"},
-]
+if not os.path.isfile('books.db'):
+    db.connect()
 
-def _find_next_id():
-    return max([user["id"] for user in users]) + 1
 
-@app.get("/users")
-def get_users():
-    return jsonify(users)
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-@app.post("/users")
-def add_user():
-    if request.is_json:
-        user = request.json
-        if("name" not in user):
-            return {"error": "Missing name"}, 400
+def isValid(email):
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if re.fullmatch(regex, email):
+      return True
+    else:
+      return False
+    
+@app.route("/request", methods=['POST'])
+def postRequest():
+    req_data = request.get_json()
+    email = req_data['email']
+    if not isValid(email):
+      return jsonify({
+         'status': '422',
+         'res': 'failure',
+         'error': 'Invalid email'})
+    title = req_data['title']
+    bks = [b.serialize() for b in db.view()]
+    for b in bks:
+      if b['title'] == title:
+         return jsonify({
+            'status': '404',
+            'res': 'failure',
+            'error': 'Book already exists'})
+      
+    bk = Book(db.getNewId(), True, title, datetime.datetime.now())
+    print('new book', bk.serialize())
+    db.insert(bk)
 
-        user["id"] = _find_next_id()
-        users.append(user)
-        return jsonify(user), 201
-    return {"error": "Request must be JSON"}, 415
+    new_bks = [b.serialize() for b in db.view()]
+    print('new book in lin:', new_bks)
+    return jsonify({
+       'res': bk.serialize(),
+         'status': '200',
+         'message': 'Book added successfully'})
 
-#$env:FLASK_APP="api.py'
-# pyhton -m flask run
+@app.route("/request", methods=['GET'])
+def getRequest():
+   content_type = request.headers.get('Content-Type')
+   bks =[b.serialize() for b in db.view()]
+   if(content_type == 'application/json'):
+      json = request.json
+      for b in bks:
+         if b['id'] == int(json['id']):
+            return jsonify({
+               'res': b,
+               'status': '200',
+               'message': 'Book found successfully'})
+         return jsonify({
+                'status': '404',
+                'res': 'failure',
+                'error': 'Book not found'})
+   else:
+      return jsonify({
+            'res': bks,
+            'status': '200',
+            'msg': 'Books found successfully',
+            'no_of_books': len(bks)})
+   
+@app.route("/request/<id>", methods=['GET'])
+def getRequestById(id):
+   req_args = request.view_args
+   bks =[b.serialize() for b in db.view()]
+   if req_args:
+    for b in bks:
+        if b['id'] == int(req_args['id']):
+            return jsonify({
+                'res': b,
+                'status': '200',
+                'message': 'Book found successfully'})
+    return jsonify({
+            'status': '404',
+            'res': 'failure',
+            'error': 'Book not found'})
+   
+@app.route("/request/", methods=['PUT'])
+def putRequest():
+   req_data = request.get_json()
+   available = req_data['available']
+   title = req_data['title']
+   id = req_data['id']
+   bks =[b.serialize() for b in db.view()]
+   for b in bks:
+      if b['id'] == id:
+            bk = Book(id, available, title, datetime.datetime.now())
+            db.update(bk)
+            return jsonify({
+                    'res': bk.serialize(),
+                    'status': '200',
+                    'message': 'Book updated successfully'})
+   return jsonify({
+            'status': '404',
+            'res': 'failure',
+            'error': 'Book not found'})
+
+@app.route("/request/<id>", methods=['DELETE'])
+def deleteRequest(id):
+   req_args = request.view_args
+   bks =[b.serialize() for b in db.view()]
+   if req_args:
+    for b in bks:
+        if b['id'] == int(req_args['id']):
+            db.delete(int(req_args['id']))
+            return jsonify({
+                'res': b,
+                'status': '200',
+                'message': 'Book deleted successfully'})
+    return jsonify({
+            'status': '404',
+            'res': 'failure',
+            'error': 'Book not found'})
