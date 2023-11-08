c81d65c536802ac3b89be0f4c3fb0b13f0a88c54
Michał Panfil <it.panfil@gmail.com>
2023-11-08 22:06:35 +0100

    crud continuation

--- eShopApp/orderApp/urls.py
+++ eShopApp/orderApp/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("products", views.products, name="products"),
+    path("products/<int:id>", views.productsDetails, name="productsDetails"),
+    path("products/create", views.create_product, name="productsCreate"),
     path("orders", views.orders, name="orders"),
     path("about", views.about, name="about"),
 
--- eShopApp/orderApp/views.py
+++ eShopApp/orderApp/views.py
@@ -1,24 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.serializers import serialize
 
 from django.http import HttpResponse
+from flask import redirect
 
 from .models import Product
+from .forms import ProductForm
 
 def home(request):
     return render(request, "pages/home.html", {})
 
 def products(request):
     products = Product.objects.all()
+    return render(request, "pages/productsList.html", {"products": products})
 
-    
-    # Serializing the product object to JSON
-    product_json = serialize('json', products)
+def productsDetails(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, "pages/productDetails.html", {"product": product})
 
-    return render(request, "pages/productsList.html", {"products": products, "jsonprd": product_json})
+def productsCreate(request):
+    return render(request, "pages/productCreate.html", {})
 
 def orders(request):
     return render(request, "pages/orders.html", {})
 
 def about(request):
     return render(request, "pages/about.html", {})
+
+
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('products')  # Assuming you have a url named 'product_list' to redirect to
+    else:
+        form = ProductForm()
+    return render(request, 'pages/productCreate.html', {'form': form})
